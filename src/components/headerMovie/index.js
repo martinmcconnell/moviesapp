a3023abc35c1b19fe9f95bf2b26906693a2f40a6
shift-runstop <shift-runstop@0xFF>
2021-11-09 04:25:57 +0000

    Fix backward/forward arrows.

--- src/components/headerMovie/index.js
+++ src/components/headerMovie/index.js
@@ -6,6 +6,7 @@ import Paper from "@material-ui/core/Paper";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import HomeIcon from "@material-ui/icons/Home";
+import { withRouter } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,13 +21,12 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const MovieHeader = (props) => {
+const MovieHeader = ( { movie, history}) => {
   const classes = useStyles();
-  const movie = props.movie;
 
   return (
     <Paper component="div" className={classes.root}>
-      <IconButton aria-label="go back">
+      <IconButton aria-label="go back" onClick={() => history.goBack()} >
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
 
@@ -38,11 +38,64 @@ const MovieHeader = (props) => {
         <br />
         <span className={classes.tagLine}>{`   "${movie.tagline}"`} </span>
       </Typography>
-      <IconButton aria-label="go forward">
+      <IconButton aria-label="go forward" onClick={() => history.goForward() } >
         <ArrowForwardIcon color="primary" fontSize="large" />
       </IconButton>
     </Paper>
   );
 };
 
-export default MovieHeader;
+export default withRouter(MovieHeader);
+
+
+
+
+
+// import React from "react";
+// import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+// import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
+// import { makeStyles } from "@material-ui/core/styles";
+// import Paper from "@material-ui/core/Paper";
+// import IconButton from "@material-ui/core/IconButton";
+// import Typography from "@material-ui/core/Typography";
+// import HomeIcon from "@material-ui/icons/Home";
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     display: "flex",
+//     justifyContent: "space-around",
+//     flexWrap: "wrap",
+//     padding: theme.spacing(1.5),
+//     margin: 0,
+//   },
+//   tagLine: {
+//     fontSize: "1.5rem",
+//   },
+// }));
+
+// const MovieHeader = (props) => {
+//   const classes = useStyles();
+//   const movie = props.movie;
+
+//   return (
+//     <Paper component="div" className={classes.root}>
+//       <IconButton aria-label="go back">
+//         <ArrowBackIcon color="primary" fontSize="large" />
+//       </IconButton>
+
+//       <Typography variant="h4" component="h3">
+//         {movie.title}
+//         <a href={movie.homepage}>
+//           <HomeIcon color="primary" />
+//         </a>
+//         <br />
+//         <span className={classes.tagLine}>{`   "${movie.tagline}"`} </span>
+//       </Typography>
+//       <IconButton aria-label="go forward">
+//         <ArrowForwardIcon color="primary" fontSize="large" />
+//       </IconButton>
+//     </Paper>
+//   );
+// };
+
+// export default MovieHeader;
